f1ad9b779897a0e45ce27054a1d07ccf78222238
MankX58 <manuelguvi500@gmail.com>
2023-12-31 22:51:43 -0500

    fixed guy position, text wrap, and ammount of scroll by click the button

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ const buttonRight = document.getElementById("slideRight");
 const buttonLeft = document.getElementById("slideLeft");
 
 buttonRight.onclick = function () {
-  document.getElementById("container").scrollLeft += 125;
+  document.getElementById("container").scrollLeft += 175;
   behavior: "smooth";
 };
 
 buttonLeft.onclick = function () {
-  document.getElementById("container").scrollLeft -= 125;
+  document.getElementById("container").scrollLeft -= 175;
   this.scroll({ top: 0, right: 125, behavior: "smooth" });
 };
